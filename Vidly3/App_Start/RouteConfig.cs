deb468d087dcaf45150565eba94f7bd9cf8968f2
Kevin <kevinluch@gmail.com>
2022-12-31 11:33:23 -0600

    edited files

--- Vidly3/App_Start/RouteConfig.cs
+++ Vidly3/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Vidly3
@@ -12,6 +8,12 @@ namespace Vidly3
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" },
+                new { year = @"\d{4}", month = @"\d{2}" });
 
             routes.MapRoute(
                 name: "Default",
--- Vidly3/Controllers/MoviesController.cs
+++ Vidly3/Controllers/MoviesController.cs
@@ -11,5 +11,25 @@ namespace Vidly3.Controllers
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
         }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
